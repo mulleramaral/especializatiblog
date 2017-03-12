00f9a25fded3951081329edd3a2af967c433d4a7
miiller caetano do amaral <miilleramaral@gmail.com>
2017-03-12 17:22:48 -0300

    Header

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Blog EspecializaTi</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Css Personalizado -->
+        <link rel="stylesheet" href="css/especializati.css">
+
+        <!-- Css Personalizado -->
+        <link rel="stylesheet" href="css/especializatireset.css">
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="imgs/favicon.png">
+    </head>
+<body>
+    <header class="top">
+        <div class="container">
+            <div class="logo">
+                <img src="imgs/logo-especializati.png" alt="EspecializaTi" class="logo">
+            </div>
+
+            <div class="form">
+                <form action="" class="form form-search form-inline">
+                    <input type="text" name="pesquisar" placeholder="Pesaquisar" class="form-control">
+                    <button>
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </header>
+
+    <!-- Final do Header -->
+
+
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
